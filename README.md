7ea89b7562a74a8616507f4caf8c92c8eb8e9587
mabelEain <mabeleain90@gmail.com>
2023-12-01 01:04:51 +0630

    updated portfolio project and first deploy

---
+++ README.md
@@ -0,0 +1,10 @@
+# Getting Started with Portfolio APP
+
+`npm i` to install all dependencies
+
+## Available Scripts
+
+When your node server started and database ready
+In the project directory, you can run:
+
+### `npm start`
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-5xl font-bold text-[#ccd6f6]">
+          Eain Phyo
+        </h1>
+        <h2 className="text-4xl sm:text-5xl font-bold text-[#8892b0]">
+          I'm a Software Engineer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am an experienced React Developer with hands-on experience in
+          identifying web-based user interactions along with designing &
+          implementing highly-responsive user interface components by deploying
+          React concepts. Proficient in translating designs & wireframes into
+          high-quality code, and writing application interface code via ReactJS
+          workflows.
+        </p>
+        <div>
+          <Link to="contact" smooth={true} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              Contact Me
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
